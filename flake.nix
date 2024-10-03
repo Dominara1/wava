1320c9e8ad2753349dab8342e5dae4e5895090c5
Dominara1 <archuseris2@gmail.com>
2024-10-03 17:59:20 +0000

    Initial NixOS support.

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+    inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    wava = {
+      url = "github:/Dominara1/wava";
+    };
+  };
+  outputs = { nixpkgs, wava, ... }: wava.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      lightgbm-cli = (with pkgs; stdenv.mkDerivation {
+          pname = "wava";
+          version = "0.2";
+          src = fetchgit {
+            url = "https://github.com/Dominara1/wava";
+            rev = "v0.2";
+            sha256 = lib.fakeSha256;
+            fetchSubmodules = true;
+          };
+          nativeBuildInputs = [
+            gcc
+            cmake
+          ];
+#buildPhase = "make -j $NIX_BUILD_CORES";
+          buildlPhase = ''
+            mkdir -p build-nix
+            cd build-nix
+            cmake ..
+            make -j $(npric)
+          '';
+        }
+      );
+    in rec {
+      defaultApp = wava.lib.mkApp {
+        drv = defaultPackage;
+      };
+      defaultPackage = wava;
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          wava
+        ];
+      };
+    }
+  );
+}
